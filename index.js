62ea32e7729e7039a35eb3ee670e39e968ad0618
karthik-karunanithi <karthik.karunanithi@aventragroup.com>
2023-03-14 17:44:29 +0530

    update the latest source

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+var error_file_loc =
+  "D:/API Project/Genivix/create_tbl_script/create_tbl_script/files/err_result.xlsx";
+var get_tbl_loc_file =
+  "D:/API Project/Genivix/create_tbl_script/create_tbl_script/files/table_list.xlsx";
 var sql = require("mssql");
 const readExcel = require("read-excel-file/node");
 const reader = require("xlsx");
-const err_result = reader.readFile("D:/DOP/err_result.xlsx");
+const err_result = reader.readFile(error_file_loc);
 
 var config = {
   user: "bahriadmin",
   password: "Xuqo1030$%123&",
   server: "ba-prod-asw-ondemand.sql.azuresynapse.net",
-  database: "corporate_reports_hub",
+  database: "tms_report_hub",
   options: {
     encrypt: true, // for azure
-    trustServerCertificate: false // change to true for local dev / self-signed certs
-  }
+    trustServerCertificate: false, // change to true for local dev / self-signed certs
+  },
 };
-var bu_name = "corporate";
-var data_source = "corsource";
+var bu_name = "special project";
+var data_source = "tmssource";
 var pen_tbl = [];
 var tbl_name = "";
 var err_tbl = [];
@@ -25,7 +29,7 @@ sql.connect(config, async function(err) {
 
   var request = new sql.Request();
 
-  readExcel("D:/DOP/table_list.xlsx").then(async data => {
+  readExcel(get_tbl_loc_file).then(async data => {
     try {
       for (i in data) {
         tbl_name = data[i][1];
@@ -35,7 +39,7 @@ sql.connect(config, async function(err) {
 
         if (script != "-1") {
           var result = await request.query(script);
-          console.log(data[i][1] + " external table is created...");
+          console.log(data[i][1] + " " + result.recordset[0]["res"]);
           // console.log(result);
         } else {
           console.log(data[i][1] + " create external table is failed..");
@@ -67,17 +71,32 @@ async function create_tbl_script(data) {
     if (columns == "-1") return "-1";
 
     var tbl_credentials =
-      "with ( location = 'dbo/" +
+      "with ( location = '@SCHEMA/" +
       data[1].toLowerCase() +
       "/**', data_source = [" +
       data_source +
       "] , file_format = [SynapseDelimitedTextFormat] ); ";
 
-    if (bu_name.toLowerCase() == "corporate")
-      tbl_credentials = tbl_credentials.replace(
-        "dbo",
-        data[1].split("_")[0].toLowerCase()
-      );
+    switch (bu_name.toLowerCase()) {
+      case "corporate":
+        tbl_credentials = tbl_credentials.replace(
+          "@SCHEMA",
+          data[1].split("_")[0].toLowerCase()
+        );
+        break;
+
+      case "liner":
+        tbl_credentials = tbl_credentials.replace("@SCHEMA", "sce");
+        break;
+
+      case "special project":
+        tbl_credentials = tbl_credentials.replace("@SCHEMA", "tmff");
+        break;
+
+      default:
+        tbl_credentials = tbl_credentials.replace("@SCHEMA", "dbo");
+        break;
+    }
 
     var query = "";
     query = "create external table [dbo]." + "[" + tbl_name + "]";
@@ -95,6 +114,11 @@ async function create_tbl_script(data) {
     return query;
   } catch (e) {
     console.log(e);
+    err_tbl.push({
+      "Table Name": tbl_name,
+      "Error Message": String(e),
+    });
+    return "-1";
   }
 }
 
@@ -105,16 +129,67 @@ function check_col_data_types(columns) {
       for (let item of columns.split(",")) {
         item = item.trim();
 
-        if (item.split(" ")[1].includes("text")) {
-          item = item.split(" ")[0] + " varchar(max)";
-        } else if (item.split(" ")[1].includes("image")) {
-          item = item.split(" ")[0] + " varbinary(max)";
-        } else if (item.split(" ")[1].includes("-1")) {
-          item = item.split(" ")[0] + " varchar(max)";
-        } else if (item.split(" ")[1].includes("money")) {
-          item = item.split(" ")[0] + " decimal";
-        } else if (item.split(" ")[1].includes("real")) {
-          item = item.split(" ")[0] + " float";
+        if (
+          bu_name.toLowerCase() == "liner" ||
+          bu_name.toLowerCase() == "special project"
+        ) {
+          if (item.split(" ")[1].toLowerCase().includes("number")) {
+            item = item.split(" ")[0] + " float";
+          } else if (item.split(" ")[1].toLowerCase().includes("long raw")) {
+            item = item.split(" ")[0] + " varbinary(max)";
+          } else if (item.split(" ")[1].toLowerCase().includes("long")) {
+            item = item.split(" ")[0] + " varchar(max)";
+          } else if (item.split(" ")[1].toLowerCase().includes("timestamp")) {
+            item = item.split(" ")[0] + " datetime";
+          } else if (item.split(" ")[1].toLowerCase().includes("clob")) {
+            item = item.split(" ")[0] + " varchar(max)";
+          } else if (item.split(" ")[1].toLowerCase().includes("date")) {
+            item = item.split(" ")[0] + " datetime";
+          } else if (item.split(" ")[1].toLowerCase().includes("raw")) {
+            item = item.split(" ")[0] + " varbinary(max)";
+          } else if (item.split(" ")[1].toLowerCase().includes("varchar2")) {
+            item =
+              item.split(" ")[0] +
+              " varchar" +
+              (item.split(" ")[1].toLowerCase().split("varchar2")[1] == "(0)"
+                ? "(1)"
+                : item.split(" ")[1].toLowerCase().split("varchar2")[1]);
+          } else if (item.split(" ")[1].toLowerCase().includes("char")) {
+            item =
+              item.split(" ")[0] +
+              " char" +
+              (item.split(" ")[1].toLowerCase().split("char")[1] == "(0)"
+                ? "(1)"
+                : item.split(" ")[1].toLowerCase().split("char")[1]);
+          } else if (item.split(" ")[1].toLowerCase().includes("rowid")) {
+            item = item.split(" ")[0] + "	char(18)";
+          } else if (item.split(" ")[1].toLowerCase().includes("blob")) {
+            item = item.split(" ")[0] + "	varbinary(max)";
+          } else if (
+            item.split(" ")[1].toLowerCase().includes("aq_seq_no_type") ||
+            item.split(" ")[1].toLowerCase().includes("aq_wip_no_type") ||
+            item.split(" ")[1].toLowerCase().includes("aq_omq_type") ||
+            item.split(" ")[1].toLowerCase().includes("anydata") ||
+            item.split(" ")[1].toLowerCase().includes("addr_obj")
+          ) {
+            err_tbl.push({
+              "Table Name": tbl_name,
+              "Error Message": "The column data type not supported",
+            });
+            return "-1";
+          }
+        } else {
+          if (item.split(" ")[1].includes("text")) {
+            item = item.split(" ")[0] + " varchar(max)";
+          } else if (item.split(" ")[1].includes("image")) {
+            item = item.split(" ")[0] + " varbinary(max)";
+          } else if (item.split(" ")[1].includes("-1")) {
+            item = item.split(" ")[0] + " varchar(max)";
+          } else if (item.split(" ")[1].includes("money")) {
+            item = item.split(" ")[0] + " decimal";
+          } else if (item.split(" ")[1].includes("real")) {
+            item = item.split(" ")[0] + " float";
+          }
         }
 
         arr.push(item);
@@ -124,6 +199,11 @@ function check_col_data_types(columns) {
     return arr.toString();
   } catch (e) {
     console.log(e);
+    err_tbl.push({
+      "Table Name": tbl_name,
+      "Error Message": String(e),
+    });
+    return "-1";
   }
 }
 
@@ -169,7 +249,7 @@ async function get_corporate_columns(tbl_name) {
     console.log(String(e));
     err_tbl.push({
       "Table Name": tbl_name,
-      "Error Message": String(e)
+      "Error Message": String(e),
     });
     return "-1";
   }
@@ -177,24 +257,26 @@ async function get_corporate_columns(tbl_name) {
 
 async function update_err_tbl() {
   try {
-    const d = new Date().toLocaleString("en-US", {
-      timeZone: "Asia/Kolkata",
-      hour12: false
-    });
+    if (err_tbl.length > 0) {
+      const d = new Date().toLocaleString("en-US", {
+        timeZone: "Asia/Kolkata",
+        hour12: false,
+      });
 
-    const date_time =
-      d.split(",")[0].split("/").reverse().join("") +
-      "_" +
-      d.split(",")[1].split(":").join("");
+      const date_time =
+        d.split(",")[0].split("/").reverse().join("") +
+        "_" +
+        d.split(",")[1].split(":").join("");
 
-    const ws = reader.utils.json_to_sheet(err_tbl);
-    await reader.utils.book_append_sheet(
-      err_result,
-      ws,
-      bu_name + "_" + date_time
-    );
-    await reader.writeFile(err_result, "D:/DOP/err_result.xlsx");
-    return "1";
+      const ws = reader.utils.json_to_sheet(err_tbl);
+      await reader.utils.book_append_sheet(
+        err_result,
+        ws,
+        bu_name + "_" + date_time
+      );
+      await reader.writeFile(err_result, error_file_loc);
+      return "1";
+    } else return "0";
   } catch (e) {
     console.log(e);
   }
@@ -220,9 +302,19 @@ async function update_err_tbl() {
 // ======================================================
 
 // ****oracle****
+// this not working if column length too high
 // select  table_name, LISTAGG('[' || column_name || '] ' || data_type || '(' || data_length || ')',',') within group (order by column_id)
 // FROM ALL_TAB_COLS
 // group by table_name
+
+// this is working
+// select  owner,table_name,
+// rtrim(xmlagg(xmlelement(e,'[' || column_name || '] ' || data_type || '(' || data_length || ')',', ').extract('//text()')
+// order by column_id).getclobval(),', ') columns
+// FROM ALL_TAB_COLS
+// where table_name in () order by table_name
+
+// =============================================================
 
 // select [db_name],''''+UPPER(REPLACE(table_name,'Tbl',''))+''',' as tbl_name,table_name from liner_migration_order
 // where is_active = '1'
